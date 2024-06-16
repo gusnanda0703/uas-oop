47e243301021a78b21975fd28f7a33c878e28224
tusetyawan <tusetyawan95@gmail.com>
2024-06-16 17:27:21 +0800

    MenuBuku.java

--- ReadLater/menu/MenuBuku.java
+++ ReadLater/menu/MenuBuku.java
@@ -1,17 +1,21 @@
 package menu;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.Optional;
+
 import models.Buku;
-import utils.ScreenHelper;
 import models.Menu;
-import repositories.BookRepository;
+import repositories.BukuRepository;
+import utils.ScreenHelper;
 
-public class MenuBuku extends Menu {
-    BookRepository data;
+public class MenuBuku extends Menu<Buku> {
+    BukuRepository bukuRepository;
+    MenuPenerbit menuPenerbit;
+    MenuPenulis menuPenulis;
 
-    public MenuBuku(BookRepository data) {
-        this.data = data;
+    public MenuBuku(BukuRepository bukuRepository, MenuPenerbit menuPenerbit, MenuPenulis menuPenulis) {
+        this.bukuRepository = bukuRepository;
+        this.menuPenerbit = menuPenerbit;
+        this.menuPenulis = menuPenulis;
     }
 
     @Override
@@ -19,6 +23,7 @@ public class MenuBuku extends Menu {
         int pilihan;
         do {
             ScreenHelper.clearConsole();
+
             System.out.println("+=============================================+");
             System.out.println("|                  DATA BUKU                  |");
             System.out.println("+=============================================+");
@@ -32,155 +37,143 @@ public class MenuBuku extends Menu {
             System.out.println("+---+-----------------------------------------+");
             System.out.println("| 0 | Kembali                                 |");
             System.out.println("+=============================================+");
+
             System.out.print("\nSilakan masukan pilihan anda (0...4) : ");
             pilihan = input.nextInt();
             input.nextLine();
+
             switch (pilihan) {
-                case 1:
-                    tampilData();
-                    break;
-                // case 2:
-                // tambah();
-                // break;
-                // case 3:
-                // edit();
-                // break;
-                // case 4:
-                // hapus();
-                // break;
-                case 0:
+                case 1 -> tampilData();
+                case 2 -> tambah();
+                case 3 -> edit();
+                case 4 -> hapus();
+                case 0 -> {
                     System.out.println("+=============================================+");
                     System.out.println("|            KEMBALI KE MENU UTAMA            |");
                     System.out.println("+=============================================+\n");
-                    break;
-                default:
+                }
+                default -> {
                     System.out.println("Pilihan yang anda input tidak tersedia, silakan ulangi kembali.");
                     input.next();
+                }
             }
         } while (pilihan != 0);
     }
 
     @Override
-    public void tampilData() {
+    public void tambah() {
         ScreenHelper.clearConsole();
-        if (data.size() > 0) {
+
+        System.out.println("+=============================================+");
+        System.out.println("| TAMBAH DATA BUKU                            |");
+        System.out.println("+=============================================+");
+
+        System.out.print("Judul Buku : ");
+        var judul = input.nextLine();
+        System.out.print("Penulis Buku : ");
+        var penulis = menuPenulis.pilih();
+        System.out.print("Penerbit Buku : ");
+        var penerbit = menuPenerbit.pilih();
+        System.out.print("Tahun Terbit : ");
+        int tahunTerbit = input.nextInt();
+        input.nextLine();
+
+        bukuRepository.add(new Buku(judul, penulis.get(), penerbit.get(), tahunTerbit));
+        System.out.println("Buku berhasil ditambahkan.");
+        input.nextLine();
+    }
+
+    @Override
+    public Optional<Buku> pilih() {
+        return pilih(true);
+    }
+
+    @Override
+    public Optional<Buku> pilih(boolean required) {
+        Optional<Buku> bukuDipilih = Optional.empty();
+        if (bukuRepository.size() > 0) {
+            do {
+                System.out.printf("\nBuku yang tersedia %d:\n", bukuRepository.size());
+                for (var buku : bukuRepository.getAll()) {
+                    System.out.printf("%d) %s\n", buku.getId(), buku.getJudul());
+                }
+                System.out.print("\nSilakan pilih ID buku : ");
+                var idBuku = input.nextInt();
+                bukuDipilih = Optional.ofNullable(bukuRepository.getById(idBuku));
+            } while (required && bukuDipilih.isEmpty());
+        } else {
+            System.out.println("Buku kosong.");
+        }
+        input.nextLine();
+        return bukuDipilih;
+    }
+
+    @Override
+    public void tampilData() {
+        if (bukuRepository.size() > 0) {
+            ScreenHelper.clearConsole();
+
             System.out.println("+=============================================+");
             System.out.println("|               TAMPIL DATA BUKU              |");
             System.out.println("+=============================================+");
-            for (Buku tempBuku : data.getAll()) {
+
+            for (Buku tempBuku : bukuRepository.getAll()) {
+                System.out.println("ID Buku       : " + tempBuku.getId());
                 System.out.println("Judul Buku    : " + tempBuku.getJudul());
                 System.out.println("Penulis Buku  : " + tempBuku.getPenulis().getName());
                 System.out.println("Penerbit Buku : " + tempBuku.getPenerbit().getName());
                 System.out.println("Tahun Terbit  : " + tempBuku.getTahunTerbit());
                 System.out.println("+=============================================+");
             }
-            input.nextLine();
         } else {
-            System.out.println("Data buku kosong, silakan tambahkan data.");
-            input.nextLine();
+            System.out.println("Buku kosong.");
         }
+        input.nextLine();
     }
 
     @Override
-    public int pilih() {
-        ScreenHelper.clearConsole();
-        String judulBuku = "";
-        int bukuDipilih = -1;
+    public void edit() {
+        var bukuDipilih = pilih();
+        if (bukuDipilih.isPresent()) {
+            ScreenHelper.clearConsole();
 
-        if (data.size() > 0) {
-            do {
-                System.out.println("+=============================================+");
-                System.out.println("|                  PILIH BUKU                 |");
-                System.out.println("+=============================================+");
-                for (Buku tempBuku : data.getAll()) {
-                    System.out.println("Judul Buku    : " + tempBuku.getJudul());
-                    System.out.println("Penulis Buku  : " + tempBuku.getPenulis());
-                    System.out.println("Penerbit Buku : " + tempBuku.getPenerbit());
-                    System.out.println("Tahun Terbit  : " + tempBuku.getTahunTerbit());
-                    System.out.println("+=============================================+");
-                }
+            System.out.println("+=============================================+");
+            System.out.println("|              EDIT DATA BUKU                 |");
+            System.out.println("+=============================================+");
 
-                System.out.print("Silakan pilih kode buku : ");
-                judulBuku = input.nextLine();
-                for (int i = 0; i < data.size(); i++) {
-                    if (data.getById(i).getJudul().equals(judulBuku)) {
-                        bukuDipilih = i;
-                        break;
-                    }
-                }
-            } while (bukuDipilih == -1);
+            System.out.println("Biarkan kosong jika tidak ingin mengubah data.");
+            var buku = bukuDipilih.get();
+            System.out.printf("Judul Buku    (%s) : ", buku.getJudul());
+            var judul = input.nextLine();
+            System.out.printf("Penulis Buku  (%s) : ", buku.getPenulis().getName());
+            var penulis = menuPenulis.pilih(false);
+            System.out.printf("Penerbit Buku (%s) : ", buku.getPenerbit().getName());
+            var penerbit = menuPenerbit.pilih(false);
+            System.out.printf("Tahun Terbit  (%d) : ", buku.getTahunTerbit());
+            var tahunTerbit = input.nextLine();
+
+            buku.setJudul(judul.isBlank() ? buku.getJudul() : judul);
+            buku.setPenulis(penulis.orElse(buku.getPenulis()));
+            buku.setPenerbit(penerbit.orElse(buku.getPenerbit()));
+            buku.setTahunTerbit(tahunTerbit.isBlank() ? buku.getTahunTerbit() : Integer.parseInt(tahunTerbit));
+            System.out.println("Buku berhasil diubah.");
         } else {
-            System.out.println("Data buku kosong, silakan tambahkan data.");
-            input.nextLine();
+            System.out.println("Buku tidak ditemukan");
         }
-        return bukuDipilih;
+        input.nextLine();
     }
 
-    // @Override
-    // public void tambah() {
-    // ScreenHelper.clearConsole();
-    // System.out.println("+=============================================+");
-    // System.out.println("| TAMBAH DATA BUKU |");
-    // System.out.println("+=============================================+");
-    // Buku tempBuku = new Buku();
-    // System.out.print("Judul Buku : ");
-    // tempBuku.setJudul(input.nextLine());
-    // System.out.print("Penulis Buku : ");
-    // tempBuku.setPenulis(input.nextLine());
-    // System.out.print("Penerbit Buku : ");
-    // tempBuku.setPenerbit(input.nextLine());
-    // System.out.print("Tahun Terbit : ");
-    // tempBuku.setTahunTerbit(input.nextLine());
-    // System.out.print("Jumlah Buku : ");
-    // tempBuku.setJumlah(input.nextInt());
-    // input.nextLine();
-    // data.add(tempBuku);
-    // System.out.println("+=============================================+");
-    // System.out.println("| DATA BUKU TERSIMPAN |");
-    // System.out.println("+=============================================+");
-    // input.nextLine();
-    // }
-
-    // @Override
-    // public void edit() {
-    // ScreenHelper.clearConsole();
-    // int indexBuku = pilih();
-    // if (indexBuku != -1) {
-    // Buku editBuku = data.get(indexBuku);
-    // System.out.println("+=============================================+");
-    // System.out.println("| EDIT DATA BUKU |");
-    // System.out.println("+=============================================+");
-    // System.out.print("Kode Buku : ");
-    // editBuku.setKode(input.nextLine());
-    // System.out.print("Judul Buku : ");
-    // editBuku.setJudul(input.nextLine());
-    // System.out.print("Penulis Buku : ");
-    // editBuku.setPenulis(input.nextLine());
-    // System.out.print("Penerbit Buku : ");
-    // editBuku.setPenerbit(input.nextLine());
-    // System.out.print("Tahun Terbit : ");
-    // editBuku.setTahunTerbit(input.nextLine());
-    // System.out.print("Jumlah Buku : ");
-    // editBuku.setJumlah(input.nextInt());
-    // data.set(indexBuku, editBuku);
-    // System.out.println("+=============================================+");
-    // System.out.println("| DATA BUKU TERSIMPAN |");
-    // System.out.println("+=============================================+");
-    // input.nextLine();
-    // input.nextLine();
-    // }
-    // }
-
-    // public void hapus() {
-    // ScreenHelper.clearConsole();
-    // int indexBuku = pilih();
-    // if (indexBuku != -1) {
-    // data.remove(indexBuku);
-    // System.out.println("+=============================================+");
-    // System.out.println("| DATA BUKU DIHAPUS |");
-    // System.out.println("+=============================================+");
-    // input.nextLine();
-    // }
-    // }
+    @Override
+    public void hapus() {
+        var bukuDipilih = pilih();
+        if (bukuDipilih.isPresent()) {
+            var buku = bukuDipilih.get();
+            bukuRepository.remove(buku);
+            System.out.println("Buku berhasil dihapus.");
+        } else {
+            System.out.println("Buku tidak ditemukan.");
+        }
+        input.nextLine();
+    }
 
 }
